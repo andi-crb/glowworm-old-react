1539fb490048713f9cd1da766ff57b343f13af4b
andi-crb <andicbuchanan@gmail.com>
2016-04-25 13:33:51 +1200

    added main routes

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import { Router, Route, browserHistory, IndexRoute } from 'react-router'
 import Home from './components/Home'
+import Profile from './components/Profile'
+import Recommendations from './components/Recommendations'
+import Reviews from './components/Reviews'
+import Signup from './components/Signup'
+import Stories from './components/Stories'
+import Story from './components/story'
 
 
 module.exports = (
   <Router history={browserHistory}>
     <Route path="/" component={Home}></Route>
+    <Route path="profile" component={Profile}></Route>
+    <Route path="recommendations" component={Recommendations}></Route>
+    <Route path="reviews" component={Reviews}></Route>
+    <Route path="signup" component={Signup}></Route>
+    <Route path="stories" component={Stories}></Route>
+    <Route path="story" component={Story}></Route>
   </Router>
 )
